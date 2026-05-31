e9484179f5eac7f17b2bac02ead96fc4c256fffd
ByalykT01 <byalykt@hotmail.com>
2026-05-31 14:33:17 +0200

    chore: cleanup, set main paths

--- cusho-frontend/vite.config.ts
+++ cusho-frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
@@ -6,6 +7,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
@@ -15,6 +17,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    tsconfigPaths(),
   ],
 })
 
